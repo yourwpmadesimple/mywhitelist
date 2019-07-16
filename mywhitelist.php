7923286a10a1933d66fed9426f51140309a9272a
Wayne <wayne@yourwpmadesimple.com>
2019-07-16 12:09:01 -0500

    Created Plugin Head

---
+++ mywhitelist.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Ny WhiteList
+* Plugin URI: https://github.com/yourwpmadesimple/mywhitelist.git
+* Description: This plugin allows the user to input their name and email address that will be output to a static HTML page
+* Version: 1.0
+* Author: Wayne Hatter
+* Author URI: https://yourwpmadesimple.com
+**/
