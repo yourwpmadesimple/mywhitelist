edd4a84bb96f82af9bb8b81b3285dcd1b9e6d719
Wayne <wayne@yourwpmadesimple.com>
2019-08-07 12:34:11 -0500

    Created script that enables the ability to copy the html output of the whitelisted page

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -1,29 +1,45 @@
-var j = jQuery.noConflict() 
-j(document).ready(function() {
-    j("#mywl-visual").hide();
-    j("button.visual").click(function(){
-        j("#mywl-visual").slideToggle();
-    });
-    j("#mywl-visual").change(function()
-{
+(function($) {
+    // Tabs
+    $( "#tabs" ).tabs();
+    $("<pre />", {
+        "html":   '&lt;!DOCTYPE html>\n&lt;html>\n' +
+                $("#head")
+                    .html()
+                    .replace(/[<>]/g, function(m) { return {'<':'&lt;','>':'&gt;'}[m]})
+                    .replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,'<a href="$1">$1</a>') +
+                '\n&lt;/html>',
+        "class": "prettyprint"
+    }).appendTo("#source-code");
+    
 
-    var test= time_slot.value;
+    // Higlight source content
+    $('#source-code .prettyprint').click(function() {
+        $(this).focus();
+        $(this).select();
+        document.execCommand('copy');
+      });
 
-j.ajax({
-        type: 'GET',
-        url: mywhitelist.php,
-        data: {
-            action: 'ajax_ajaxhandler',
-            test : test   
-        },
-        success: function(data) {
-            alert(data);
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            alert("Error");
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.backTop');
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('backTopIsVisible') : $back_to_top.removeClass('backTopIsVisible backTopFadeOut');
+        if( $(this).scrollTop() > offset_opacity ) { 
+            $back_to_top.addClass('backTopFadeOut');
         }
     });
-
-    return false;
-  });
-});
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0 ,
+            }, scroll_top_duration
+        );
+    });
+}(jQuery));
--- index.php
+++ index.php
@@ -22,30 +22,34 @@ function mywl_settings_link( $links ) {
 	return $links;
 }
 
-
+//add_action( 'admin_init','mywhitelist');
 function mywhitelist() {
-    wp_register_style('mywhitelist', plugins_url('/includes/css/style.css',__FILE__ ));
-    wp_enqueue_style('mywhitelist');
-    wp_register_script( 'mywl-js', plugins_url('/includes/js/scripts.js',__FILE__ ));
-    wp_enqueue_script('mywl-js');
+		wp_enqueue_style('mywl-css', plugins_url('/includes/css/style.css',__FILE__ ));
+		wp_enqueue_style('mywl-ui-base-theme', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
+
+		wp_enqueue_script( 'mywl-js', plugins_url('/includes/js/scripts.js',__FILE__ ), array('jquery'), true);
+		wp_enqueue_script( 'mywl-tabs', 'https://code.jquery.com/ui/1.12.1/jquery-ui.js', true);
 }
-
-add_action( 'admin_init','mywhitelist');
-
 // Options Page
 
 add_action( 'admin_menu', 'mywl_add_admin_menu' );
-add_action( 'admin_init', 'mywl_settings_init' );
-
-
 function mywl_add_admin_menu(  ) { 
 
 	add_menu_page( 'My WhiteList', 'My WhiteList', 'manage_options', 'my_whitelist', 'mywl_options_page' );
-
+	add_submenu_page( 'my_whitelist', 'My Custom Page', 'My Custom Page',
+    'manage_options', 'my_whitelist_code_page', 'whitelist_page_code');
 }
 
+function whitelist_page_code(){
+?>
+<div id="page-code">
+	<?php include('includes/mywhitelist.html'); ?>
+</div>
+<?php
+}
 
-function mywl_settings_init(  ) { 
+add_action( 'admin_init', 'mywl_settings_init' );
+function mywl_settings_init() { 
 
 	register_setting( 'pluginPage', 'mywl_settings' );
 
@@ -139,16 +143,6 @@ function mywl_options_page(  ) {
 			?>
 
 		</form>
-        <?php
-echo '<button class="visual">View Visual Page</button>';
-echo '<button class="html">View HTML Code</button>';
-echo '<div id="mywl-html">';  
-show_source("includes/mywhitelist.php");
-echo "</div>";
-echo '<div id="mywl-visual">';
-include "includes/mywhitelist.php";
-echo "</div>";
-
-
-
+		<?php include('includes/mywhitelist.php'); ?>
+<?php
 }
